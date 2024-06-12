6811ad1a3d78b49f453e517610d080950849f989
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2024-06-11 23:47:40 -0300

    add english content

--- examples/login-nextjs/src/app/connect/[providerName]/login-redirect.front.js
+++ examples/login-nextjs/src/app/connect/[providerName]/login-redirect.front.js
@@ -39,7 +39,7 @@ export const LoginRedirectFront = (props) => {
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Login Status
+          Sign in to your account
         </h2>
       </div>
 
@@ -47,7 +47,7 @@ export const LoginRedirectFront = (props) => {
         <form class="space-y-6" action="#" method="POST">
 
           <p className='text-center text-gray-900'>
-            Login realizado com sucesso.
+            Login succcess 
           </p>
         </form>
 
